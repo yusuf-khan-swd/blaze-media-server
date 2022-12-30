196229f4a385390b63f45a76175325b3571c2f5c
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2022-12-30 23:53:19 +0600

    add post to db

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 
 const app = express();
 app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.tjl9nwy.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+  try {
+    const postsCollection = client.db("blazeMedia").collection("posts");
+
+    app.post("/posts", async (req, res) => {
+      const post = req.body;
+      const result = await postsCollection.insertOne(post);
+      res.send(result);
+    });
+  }
+  finally {
+
+  }
+
+}
+
+run().catch(console.log);
 
 app.get("/", (req, res) => {
   res.send("Blaze media server is running");
